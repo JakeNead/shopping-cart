6f6e1ac352f0dcf2bf13486b4cd4fb446f5b2c98
Jake Nead <jakenead@gmail.com>
2024-04-20 20:33:41 -0700

    Pass addToCart prop tgo ItemCard

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,15 +3,17 @@ import ItemCard from "./ItemCard";
 import "./shop.css";
 
 export default function Shop() {
-  const [inventory, error, loading] = useOutletContext();
+  const [inventory, error, loading, addToCart] = useOutletContext();
   if (error) return <p>A network error was encountered</p>;
   if (loading) return <p>Loading...</p>;
 
   return (
     <main>
-      {inventory.map((obj) => (
-        <ItemCard key={obj.id} item={obj} />
-      ))}
+      <div className="inventoryContainer">
+        {inventory.map((obj) => (
+          <ItemCard key={obj.id} item={obj} addToCart={addToCart} />
+        ))}
+      </div>
     </main>
   );
 }
