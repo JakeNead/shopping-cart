a8e2b17898a5398c1876d7976e17dbff0f24cae2
Jake Nead <jakenead@gmail.com>
2024-04-23 14:23:30 -0700

    Initial cart page render

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,3 +1,58 @@
+import CartCard from "./CartCard";
+import { useOutletContext, Link } from "react-router-dom";
+import "./cart.css";
+
 export default function Cart() {
-  return <div>cart page</div>;
+  const { cart, cartTotal, handleCartChange, handleCartDelete } =
+    useOutletContext();
+
+  function subTotalPrice() {
+    if (cart) {
+      let price = cart.reduce((a, c) => a + c.quantity * c.price, 0);
+      return price;
+    }
+  }
+  return (
+    <main>
+      {cart.length === 0 ? (
+        <>
+          <h2>Your Shopping Cart is empty</h2>
+          <p>
+            Your cart currently has nothing in it. Which means there's plenty of
+            room for all of the amazing items you could possibly want!
+          </p>
+          <p>
+            If you have a FakeStore account, be sure to <b>Sign In </b>to see
+            your personal shopping cart.
+          </p>
+          <Link to={"shop"}>
+            <button>Continue Shopping</button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <div className="cartItems">
+            {cart.map((item) => (
+              <CartCard
+                key={item.id}
+                item={item}
+                handleChange={handleCartChange}
+                handleDelete={handleCartDelete}
+              />
+            ))}
+          </div>
+          <div className="orderSummary">
+            <div className="subtotal">
+              <p>Subtotal ({cartTotal()} items)</p>
+              <p>${subTotalPrice()}</p>
+            </div>
+            <div className="shipping">
+              <p>Shipping</p>
+              <p>{subTotalPrice() > 50 ? "free" : "$9.95"}</p>
+            </div>
+          </div>
+        </>
+      )}
+    </main>
+  );
 }
--- src/components/cart/cartCard.jsx
+++ src/components/cart/cartCard.jsx
@@ -1,5 +1,31 @@
 import "./cartCard.css";
 
-export default function CartCard() {
-  return <article></article>;
+export default function CartCard({ handleChange, handleDelete, item }) {
+  return (
+    <article>
+      <div className="cardTop">
+        <div className="imageContainer">
+          <img src={item.image} alt={item.title} />
+        </div>
+        <div className="itemInfo">
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+        </div>
+      </div>
+
+      <form action="" data-id={item.id} onSubmit={handleDelete}>
+        <label htmlFor="quantity">
+          <input
+            type="number"
+            id="quantity"
+            defaultValue={item.quantity}
+            min="1"
+            max="10"
+            onChange={handleChange}
+          />
+        </label>
+        <button>Delete</button>
+      </form>
+    </article>
+  );
 }
