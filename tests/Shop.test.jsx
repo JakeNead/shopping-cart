3450c91ee615feb0002e0aa8f23870eff389a7db
Jake Nead <jakenead@gmail.com>
2024-05-09 12:28:01 -0700

    Remove redundant mock inventory properties

--- tests/Shop.test.jsx
+++ tests/Shop.test.jsx
@@ -29,7 +29,7 @@ describe(Shop, () => {
         <Shop />
       </RenderRouteWithOutletContext>
     );
-    expect(screen.getByRole("heading", { name: "Foo" })).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: "foo" })).toBeInTheDocument();
   });
 });
 
@@ -46,70 +46,14 @@ function RenderRouteWithOutletContext({ mockContext, children }) {
 }
 
 const mockErrorContext = {
-  inventory: [
-    {
-      id: 1,
-      title: "Foo",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: {
-        rate: 3.9,
-        count: 120,
-      },
-    },
-    {
-      id: 2,
-      title: "Bar",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
-      image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      rating: {
-        rate: 4.1,
-        count: 259,
-      },
-    },
-  ],
+  inventory: [],
   error: true,
   loading: false,
   cart: [],
 };
 
 const mockLoadingContext = {
-  inventory: [
-    {
-      id: 1,
-      title: "Foo",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: {
-        rate: 3.9,
-        count: 120,
-      },
-    },
-    {
-      id: 2,
-      title: "Bar",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
-      image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      rating: {
-        rate: 4.1,
-        count: 259,
-      },
-    },
-  ],
+  inventory: [],
   error: false,
   loading: true,
   cart: [],
@@ -119,29 +63,14 @@ const mockSuccessContext = {
   inventory: [
     {
       id: 1,
-      title: "Foo",
+      title: "foo",
       price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
+      description: "bar",
+      category: "baz",
       image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
       rating: {
         rate: 3.9,
         count: 120,
-      },
-    },
-    {
-      id: 2,
-      title: "Bar",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
-      image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      rating: {
-        rate: 4.1,
-        count: 259,
       },
     },
   ],
