07a3e257a43eda5f595e72aeb523fa068a706560
Jake Nead <jakenead@gmail.com>
2024-05-11 20:09:35 -0700

    Cart testing complete

--- tests/Cart.test.jsx
+++ tests/Cart.test.jsx
@@ -1,31 +1,40 @@
-import { Outlet, RouterProvider, createMemoryRouter } from "react-router-dom";
-import { it, vi } from "vitest";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Cart from "../src/components/cart/Cart";
 
 describe("Cart", () => {
   it("should display a notification when cart is empty", () => {
-    const router = createMemoryRouter(mockedRoutes({ cart: [] }));
+    const router = createBrowserRouter(mockedRoutes({ cart: [] }));
     render(<RouterProvider router={router} />);
     expect(screen.getByRole("heading", { name: /empty/ })).toBeInTheDocument();
   });
 
   it("should display a single item", () => {
-    const router = createMemoryRouter(mockedRoutes(singleItemMockContext));
+    const router = createBrowserRouter(mockedRoutes(singleItemMockContext));
     render(<RouterProvider router={router} />);
     expect(screen.getByRole("heading", { name: /foo/ })).toBeInTheDocument();
   });
 
   it("should display a multiple items", () => {
-    const router = createMemoryRouter(mockedRoutes(twoItemsMockContext));
+    const router = createBrowserRouter(mockedRoutes(twoItemsMockContext));
     render(<RouterProvider router={router} />);
     expect(screen.getByRole("heading", { name: /foo/ })).toBeInTheDocument();
     expect(screen.getByRole("heading", { name: /bar/ })).toBeInTheDocument();
   });
-  it("should update price when quantity changes", () => {});
-  //   it("should let user delete items", () => {});
-  //   it("should include shipping costs when cart is under $50", () => {});
-  //   it("should include free shipping when cart is over $50", () => {});
+
+  it("should include shipping costs when cart is under $50", () => {
+    const router = createBrowserRouter(mockedRoutes(singleItemMockContext));
+    render(<RouterProvider router={router} />);
+    expect(screen.getByText("$5.00")).toBeInTheDocument();
+    expect(screen.getByText("$9.95")).toBeInTheDocument();
+  });
+
+  it("should include free shipping when cart is over $50", () => {
+    const router = createBrowserRouter(mockedRoutes(twoItemsMockContext));
+    render(<RouterProvider router={router} />);
+    expect(screen.getByText("free")).toBeInTheDocument();
+  });
 });
 
 function mockedRoutes(mockContext) {
@@ -44,8 +53,9 @@ const singleItemMockContext = {
     {
       id: 1,
       title: "foo",
-      price: 109.95,
+      price: 5,
       image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      quantity: 1,
     },
   ],
   cartTotal: vi.fn(),
@@ -61,12 +71,14 @@ const twoItemsMockContext = {
       title: "foo",
       price: 109.95,
       image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      quantity: 1,
     },
     {
       id: 2,
       title: "bar",
       price: 23.0,
       image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      quantity: 1,
     },
   ],
   cartTotal: vi.fn(),
